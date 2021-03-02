320aec2244dc5f272463f3820ae3ce17940fdfed
amerucci <a.merucci@onlineformapro.com>
2021-03-02 09:51:24 +0100

    before compiling

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -7,26 +7,24 @@
     <!-- @save-memory="saveMemory"  @submit.prevent="submitForm" -->
     <form class="ion-padding" @submit.prevent="submitForm">
       <ion-list>
+        <ion-label position="floating"> Title </ion-label>
+        <ion-input type="text" required v-model="enteredTitle"></ion-input>
       
-          <ion-label position="floating"> Title </ion-label>
-          <ion-input type="text" required v-model="enteredTitle"></ion-input>
-       
-       
-            <ion-img :src="showImgSelected" :alt="imagesSelected"></ion-img>
-         
-        
-      
-          <input
-            type="file"
-            style="display: none"
-            @change="onFileSelected"
-            ref="fileInput"
-            id="toto"
-          />
-          <ion-button @click="$refs.fileInput.click()" expand="full"
-            >Choisir une image</ion-button
-          >
-       
+        <ion-input type="text" v-model="enteredImageUrl">{{ toto }}</ion-input>
+
+     
+        <img :src="enteredImageUrl" />
+        <input
+          type="file"
+          style="display: none"
+          @change="onFileSelected"
+          ref="fileInput"
+          id="toto"
+        />
+        <ion-button @click="$refs.fileInput.click()" expand="full"
+          >Choisir une image</ion-button
+        >
+
         <ion-item>
           <ion-label position="floating"> Description </ion-label>
           <ion-textarea
@@ -35,19 +33,18 @@
             v-model="enteredDescription"
           ></ion-textarea>
         </ion-item>
-        <ion-item>
-          <ion-datetime
-            display-format="MM DD YY"
-            placeholder="Select Date"
-          ></ion-datetime>
-        </ion-item>
+     
         <ion-button type="submit" expand="full">Ajouter un membre</ion-button>
       </ion-list>
     </form>
   </base-layout>
+
+
 </template>
 
 <script>
+
+
 import {
   IonList,
   IonItem,
@@ -55,8 +52,8 @@ import {
   IonInput,
   IonTextarea,
   IonButton,
-  IonDatetime,
-  IonImg
+
+
 } from "@ionic/vue";
 
 export default {
@@ -67,8 +64,8 @@ export default {
     IonInput,
     IonTextarea,
     IonButton,
-    IonDatetime,
-    IonImg
+
+
   },
   data() {
     return {
@@ -79,44 +76,35 @@ export default {
       selectedFile: null,
     };
   },
-  watch: {
-    imagesSelected(){
-      if (localStorage.getItem('images')){
-this.enteredImageUrl = JSON.parse(localStorage.getItem('images'))
-      }  else{
-       this.enteredImageUrl =  "https://semantic-ui.com/images/wireframe/image.png"
-      } 
-    }
-  },
-computed:{
-  showImgSelected(){
-    this.enteredImageUrl =  JSON.parse(localStorage.getItem("images"))
-  }
-},
+
  
+
   methods: {
+
+
+  
+
+
     onFileSelected(event) {
+	
+
       
+		
+
+
       let reader = new FileReader();
       reader.onload = function () {
-        //alert(reader.result);
         localStorage.setItem("images", JSON.stringify(reader.result));
-       
-      
-    
-        
       };
       if (event.target.files[0]) {
         reader.readAsDataURL(event.target.files[0]);
-       
-            localStorage.removeItem('images');
-           
+
+        localStorage.removeItem("images");
       }
-     
     },
     submitForm() {
-      alert(this.enteredImageUrl)
-      
+   
+
       this.members.push({
         id: new Date().toISOString(),
         title: this.enteredTitle,
@@ -126,10 +114,12 @@ computed:{
       localStorage.setItem("members", JSON.stringify(this.members));
       this.$store.commit("increment");
       this.$router.replace("/memories");
-      localStorage.removeItem('images');
-      localStorage.removeItem('members');
-     
+      localStorage.removeItem("images");
+      localStorage.removeItem("members");
     },
   },
+
+
+  
 };
 </script>
